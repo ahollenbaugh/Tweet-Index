f3f584a618c152b6120f6f96dedbfd85371e08d0
Allie Hollenbaugh <aghollenbaugh@gmail.com>
2021-10-15 21:37:40 -0700

    add bucket sort

--- tweet-index.py
+++ tweet-index.py
@@ -5,15 +5,57 @@
 # 4. Make all words lowercase
 
 import csv
+from enum import Enum, auto
+import sys
+
+
+class AutoName(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        start = 0
+        return count
+
+
+class Bucket(AutoName):
+    BUCKET_A = auto()
+    BUCKET_B = auto()
+    BUCKET_C = auto()
+    BUCKET_D = auto()
+    BUCKET_E = auto()
+    BUCKET_F = auto()
+    BUCKET_G = auto()
+    BUCKET_H = auto()
+    BUCKET_I = auto()
+    BUCKET_J = auto()
+    BUCKET_K = auto()
+    BUCKET_L = auto()
+    BUCKET_M = auto()
+    BUCKET_N = auto()
+    BUCKET_O = auto()
+    BUCKET_P = auto()
+    BUCKET_Q = auto()
+    BUCKET_R = auto()
+    BUCKET_S = auto()
+    BUCKET_T = auto()
+    BUCKET_U = auto()
+    BUCKET_V = auto()
+    BUCKET_W = auto()
+    BUCKET_X = auto()
+    BUCKET_Y = auto()
+    BUCKET_Z = auto()
+
 
 DEBUG = True
 FILENAME = "trump-tweets.csv"
 CHARS_REMOVE_FROM_WORD = set('.,!?\"^&*()\n_+=[]{};:\'<>~`“”✔’🔥🏆‘-') # if found in a word, remove from word
 CHARS_REMOVE_WORD_ITSELF = set('@#$%\\/0123456789') # if found in word, throw out that word (check this first)
+ALLOWED_CHARACTERS = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
 # Example: @ mentions, URLs, numbers, prices, percents, hashtags (avoids having to separate words)
+SIZE = 26
+ALPHABET = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 
 # 1. Create dictionary to store words and their ID:
 word_dict = dict()
+word_dict_size = 0
 
 print("Working...")
 
@@ -34,25 +76,124 @@ with open(FILENAME, mode='r', encoding="utf8") as csv_file:
             for word in tweet_word_list:
                 throw_away = False
                 for letter in word:
-                    for char in CHARS_REMOVE_WORD_ITSELF:
-                        if letter == char:
-                            throw_away = True
-                            break
-                    if not throw_away:
-                        for char in CHARS_REMOVE_FROM_WORD:
-                            if letter == char: # check whether or not to throw out a char
-                                word = word.replace(char,"") # remove char from word
-                    else:
+                    if letter not in ALLOWED_CHARACTERS:
+                        throw_away = True
                         break
                 # 5. Add word to word_dict and append id:
                 if not throw_away:
-                    if word not in word_dict: word_dict[word] = list() # create new list so id can be appended
+                    word = word.lower()
+                    if word not in word_dict:
+                        word_dict[word] = list() # create new list so id can be appended
+                        word_dict_size = word_dict_size + 1
                     word_dict[word].append(tweet_id) # append tweet id to list corresponding to this word
             line_count += 1
 
+# 6. Bucket sort!
+alphabet_tuple_list = []
+
+count = 0
+while count < SIZE:
+    alphabet_tuple_list.append(list())
+    count = count + 1
+
+for key in word_dict.keys():
+    if key[0] == 'a':
+        alphabet_tuple_list[0].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'b':
+        alphabet_tuple_list[1].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'c':
+        alphabet_tuple_list[2].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'd':
+        alphabet_tuple_list[3].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'e':
+        alphabet_tuple_list[4].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'f':
+        alphabet_tuple_list[5].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'g':
+        alphabet_tuple_list[6].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'h':
+        alphabet_tuple_list[7].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'i':
+        alphabet_tuple_list[8].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'j':
+        alphabet_tuple_list[9].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'k':
+        alphabet_tuple_list[10].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'l':
+        alphabet_tuple_list[11].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'm':
+        alphabet_tuple_list[12].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'n':
+        alphabet_tuple_list[13].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'o':
+        alphabet_tuple_list[14].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'p':
+        alphabet_tuple_list[15].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'q':
+        alphabet_tuple_list[16].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'r':
+        alphabet_tuple_list[17].append((key,word_dict[key]))
+        continue
+    elif key[0] == 's':
+        alphabet_tuple_list[18].append((key,word_dict[key]))
+        continue
+    elif key[0] == 't':
+        alphabet_tuple_list[19].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'u':
+        alphabet_tuple_list[20].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'v':
+        alphabet_tuple_list[21].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'w':
+        alphabet_tuple_list[22].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'x':
+        alphabet_tuple_list[23].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'y':
+        alphabet_tuple_list[24].append((key,word_dict[key]))
+        continue
+    elif key[0] == 'z':
+        alphabet_tuple_list[25].append((key,word_dict[key]))
+        continue
+
+alphabet_index = 0
+for list in alphabet_tuple_list:
+    list.sort()
+    # print(f"----------{ALPHABET[alphabet_index]} List----------\n")
+    # for item in list:
+    #     print(item, "\n")
+    alphabet_index = alphabet_index + 1
+
 if DEBUG:
-    for key, value in word_dict.items():
-        print(key, ": ", value)
+    # for key, value in word_dict.items():
+    #     print(key, ": ", value)
+    # word_dict_sorted = sorted(word_dict.items())
+    # for item in word_dict_sorted:
+    #     print(item, "\n")
+    # print(f"----------{ALPHABET[0]} List----------\n")
+    # for multipair in alphabet_tuple_list[0]:
+    #     print(multipair)
+    print(f"Number of words: {word_dict_size}")
 
 
 
